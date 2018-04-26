b924b63bdb9661d6ee8940b70c00ebca934ee799
Sasha Mikhel <mikhel@dumka.pro>
2018-04-26 19:57:03 +0300

    init

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+//Route::get('tasks', 'TasksController@index')->name('tasks.index');
+//
+//Route::get('tasks/create', 'TasksController@create')->name('tasks.create');
+//
+//Route::post('tasks/store', 'TasksController@store')->name('tasks.store');
+//
+//Route::get('tasks/{id}/edit', 'TasksController@edit')->name('tasks.edit');
+//
+//Route::put('tasks/{id}/update', 'TasksController@update')->name('tasks.update');
+//
+//Route::get('task/{id}/show', 'TasksController@show')->name('tasks.show');
+//
+//Route::delete('task/{id}/destroy', 'TasksController@destroy')->name('tasks.destroy');
+
+Route::resource('tasks', 'TasksController');
